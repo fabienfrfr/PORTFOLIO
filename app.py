b82109fc54bd072195d3810c2b299430e026894f
fabienfrfr <fabien.furfaro@gmail.com>
2022-03-02 17:31:20 +0100

    add model for dashboard in flask app

--- app.py
+++ app.py
@@ -1,14 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 17:11:24 2022
+@author: fabien
+
+https://towardsdatascience.com/web-visualization-with-plotly-and-flask-3660abf9c946
+https://youtu.be/6plVs_ytIH8
+"""
+
 from flask import Flask, render_template, request, flash
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/hello")
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/chart1')
+def chart1():
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header="Fruit in North America"
+    description = """
+    A academic study of the number of apples, oranges and bananas in the cities of
+    San Francisco and Montreal would probably not come up with this chart.
+    """
+    return render_template('notdash2.html', graphJSON=graphJSON, header=header,description=description)
+
+@app.route('/chart2')
+def chart2():
+    df = pd.DataFrame({
+        "Vegetables": ["Lettuce", "Cauliflower", "Carrots", "Lettuce", "Cauliflower", "Carrots"],
+        "Amount": [10, 15, 8, 5, 14, 25],
+        "City": ["London", "London", "London", "Madrid", "Madrid", "Madrid"]
+    })
+
+    fig = px.bar(df, x="Vegetables", y="Amount", color="City", barmode="stack")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header="Vegetables in Europe"
+    description = """
+    The rumor that vegetarians are having a hard time in London and Madrid can probably not be
+    explained by this chart.
+    """
+    return render_template('notdash2.html', graphJSON=graphJSON, header=header,description=description)
+
+@app.route("/hello")
+def hello():
 	flash("what's your name?")
-	return render_template("index.html")
+	return render_template("hello.html")
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+	return render_template("hello.html")
